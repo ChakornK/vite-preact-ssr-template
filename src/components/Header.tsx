c9280bbf95193d62b90b235b5fe1a6db79f6340c
ChakornK <chakornk2007@gmail.com>
2025-11-24 01:04:01 -0800

    feat: add lucide icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,32 @@
 import { useLocation } from "preact-iso";
+import { CircleQuestionMarkIcon, HomeIcon, SparklesIcon } from "lucide-react";
 
 export function Header() {
   const { url } = useLocation();
 
   return (
-    <div className={"bg-ctp-surface0 px-8 py-4 flex text-lg justify-between"}>
-      <p className={"font-bold"}>Preact quickstart template</p>
-      <div className={"flex gap-8 items-center"}>
-        <a href="/" class={url == "/" && "active"}>
+    <div className="flex justify-between bg-ctp-surface0 px-8 py-4 text-lg">
+      <div className="flex items-center gap-4 text-xl">
+        <SparklesIcon />
+        <p className={"font-bold"}>Preact quickstart template</p>
+      </div>
+      <div className={"flex gap-4 items-center"}>
+        <a
+          href="/"
+          className={`flex items-center gap-2 px-3 py-1.5 rounded-lg ${
+            url === "/" ? "bg-ctp-surface1" : ""
+          }`}
+        >
+          <HomeIcon size={"1em"} />
           Home
         </a>
-        <a href="/404" class={url == "/404" && "active"}>
+        <a
+          href="/404"
+          className={`flex items-center gap-2 px-3 py-1.5 rounded-lg ${
+            url === "/404" ? "bg-ctp-surface1" : ""
+          }`}
+        >
+          <CircleQuestionMarkIcon size={"1em"} />
           404
         </a>
       </div>
